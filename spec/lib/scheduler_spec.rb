159e9610e225e82b0aeb25d9d03b2f680d714786
Pawel Lorek <palorek@gmail.com>
2015-03-04 20:04:20 +0100

    Crazy test - Let's DO THIS !

--- spec/lib/scheduler_spec.rb
+++ spec/lib/scheduler_spec.rb
@@ -80,4 +80,62 @@ describe Scheduler do
     end
   end
 
+  context "with preference" do
+    let (:crazy_days) do
+      {
+        schedule: {
+          monday: [0]*5 + [1]*10 + [0]*9,
+          friday: [0]*10 + [1]*8 + [0]*6,
+          sunday: [0]*15 + [1]*3 + [0]*6
+        },
+        workers: {
+          paul: {
+            monday: [0]*12 + [2]*2 + [1]*6 + [0]*4,
+            tuesday: freeday,
+            wednesday: workday,
+            friday: workday
+          },
+          alberto: {
+            monday: [0]*4 + [1]*12 + [0]*8,
+            sunday: freeday,
+            friday: [0]*8 + [1]*12 + [0]*4
+          },
+          kuba: {
+            sunday: [0]*12 + [2]*5 + [1]*3 + [0]*4
+          }
+        }
+      }
+    end
+    let (:schedule) { Scheduler.new(crazy_days) }
+
+    let (:expectation) do
+      {
+        paul:{
+          monday: [0]*12 + [1]*3 + [0]*9,
+          tuesday: freeday,
+          wednesday: freeday,
+          friday: freeday,
+          sunday: freeday
+        },
+        alberto: {
+          monday: [0]*5 + [1]*7 + [0]*12,
+          friday: [0]*10 + [1]*8 + [0]*6,
+          sunday: freeday
+        },
+        kuba: {
+          monday: freeday,
+          friday: freeday,
+          sunday: [0]*15 + [1]*3 + [0]*6
+        }
+      }
+    end
+
+    it "returns correct output" do
+      result = schedule.run
+      expect(result[:paul]).to eq(expectation[:paul])
+      expect(result[:alberto]).to eq(expectation[:alberto])
+      expect(result[:kuba]).to eq(expectation[:kuba])
+    end
+  end
+
 end
