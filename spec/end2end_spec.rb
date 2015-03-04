865f074892254e09627e5e8a2b3d9e109694e4bd
Andrea D'Ippolito <adedip@gmail.com>
2015-03-04 19:39:28 +0100

    failing before x test

--- spec/end2end_spec.rb
+++ spec/end2end_spec.rb
@@ -56,6 +56,11 @@ class TestParser < Minitest::Test
     expected = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1]
     worker_day = Parser.convert_worker_preference_to_array(worker_day_preference)
     assert_equal expected, worker_day
+
+    worker_day_preference = "before 2 PM"
+    expected = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0]
+    worker_day = Parser.convert_worker_preference_to_array(worker_day_preference)
+    assert_equal expected, worker_day
   end
 
   def test_range_conversion_to_array
