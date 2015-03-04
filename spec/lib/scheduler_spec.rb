ac822468fc7e2598fc3e61c259cc63a535254b7c
Pawel Lorek <palorek@gmail.com>
2015-03-04 19:29:47 +0100

    Alternating days test

--- spec/lib/scheduler_spec.rb
+++ spec/lib/scheduler_spec.rb
@@ -4,7 +4,9 @@ def workday
   [0]*8 + [1]*8 + [0]*8
 end
 
-
+def freeday
+  [0]*24
+end
 
 describe Scheduler do
   let(:scheduler) { Scheduler.new }
@@ -22,6 +24,12 @@ describe Scheduler do
         }
       }
     }
+  end
+
+  WEEK = [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
+
+  def workweek(*days)
+    WEEK.reduce({}) { |result, day| result[:day] = ( days.include?(day) ? workday : freeday )  }
   end
 
   let(:multi_days) do
@@ -55,9 +63,21 @@ describe Scheduler do
     let(:schedule) { Scheduler.new(multi_workers) }
 
     it 'returns correct ouput' do
-      expect(schedule.run).to eq(paul: {monday: workday, tuesday: workday}, marc: {monday: [0]*24, tuesday: [0]*24})
+      expect(schedule.run).to eq(paul: {monday: workday, tuesday: workday}, marc: {monday: freeday, tuesday: freeday})
     end
   end
 
+  describe "different availability" do
+    let (:alternating_days) do
+      input[:schedule].merge!({tuesday: workday})
+      input[:workers][:marc] = {monday: freeday, tuesday: workday}
+      input[:workers][:paul][:tuesday]  = freeday
+      input
+    end
+    let (:schedule) { Scheduler.new(alternating_days) }
+    it "returns correct output" do
+      expect(schedule.run).to eq(paul: {monday: workday, tuesday: freeday}, marc: {monday: freeday, tuesday: workday})
+    end
+  end
 
 end
